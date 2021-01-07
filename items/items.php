0268f86f4ef14bcfd36533c0c20f2633a18ce5c8
waelabohamza <waeleagle1243@gmail.com>
2021-01-07 16:01:04 +0200

    refresh

--- items/items.php
+++ items/items.php
@@ -1,14 +1,8 @@
 <?php
-
 include "../connect.php";
-
 $data = getAllData("itemsview", "1 = 1");
-
 if ($data['count'] > 0) {
-
     echo json_encode($data['values']);
-    
 } else {
-
     zeroCount();
 }
