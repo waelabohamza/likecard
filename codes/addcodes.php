9d9ad927b8adc4612c8ab173159101d86af948e8
waelabohamza <waeleagle1243@gmail.com>
2021-01-07 21:40:56 +0200

    refresh

--- codes/addcodes.php
+++ codes/addcodes.php
@@ -3,15 +3,12 @@
 include "../connect.php";
 $table = "codes" ; 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-
     $code      =     superFilter($_POST['code']);
     $items     =     superFilter($_POST['itemid']);
-    
     $values = array(
         "codes_name"      => $code,
         "codes_items"     => $items
     );
-    
     $count = insertData($table, $values);
     countresault($count);
 
