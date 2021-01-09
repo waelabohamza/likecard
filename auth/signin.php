de29b9b117d0a8ba037d0343390d96d6c5eb3ab1
waelabohamza <waeleagle1243@gmail.com>
2021-01-09 23:23:14 +0200

    refresh

--- auth/signin.php
+++ auth/signin.php
@@ -2,6 +2,19 @@
 
 include "../connect.php" ; 
 
+$email    = superFilter($_POST['email']) ; 
+$password = sha1($_POST['password']) ; 
 
+$data     = signInWithEmailAndPassword("users" ,"users_email" , "users_password" , $email , $password);
+$users    = $data['values'] ; 
+$count    = $data['count'] ; 
 
-?>
+if ($count > 0 ) {
+  
+    echo json_encode(array("status" => "success" , "message" => $users)) ; 
+
+}else {
+  
+    echo json_encode(array("status" => "faild")) ; 
+
+}
---
+++ auth/signup.php
@@ -0,0 +1,21 @@
+<?php
+
+include  "../connect.php";
+
+$table = "users";
+
+$username = superFilter($_POST['username']);
+$password = sha1($_POST['username']);
+$email    = superFilter($_POST['email']);
+$data = getData("users", "users_email",  $email,  "AND users_password = $password");
+$count = $data['count'];
+if ($count > 0) {
+    echo json_encode(array("status" => "faild"));
+} else {
+    $values = array(
+        "users_name" => $username,
+        "users_email" => $email,
+        "users_password" => $password
+    );
+    $count  = insertData($table, $values);
+}
