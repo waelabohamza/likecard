feb1832caec7ebcc6011495f24befd6b588aa0e8
waelabohamza <waeleagle1243@gmail.com>
2021-05-31 12:04:42 +0300

    r

--- users/deleteusers.php
+++ users/deleteusers.php
@@ -5,7 +5,10 @@ $table = "users" ;
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $id = superFilter($_POST['id']);
     $count = deleteData($table, "users_id", $id);
+    if ($count > 0){
+        deleteFile($filedir , $imageold) ; 
+    }
     countresault($count);
-}else {
+}else { 
     zeroCount() ; 
 }
